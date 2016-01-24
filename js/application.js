623300f843e32af6fb1ab6c019582d484f337aeb
Shawn Alwani <shawn.alwani@gmail.com>
2016-01-24 15:16:36 +0800

    number generator added

--- js/application.js
+++ js/application.js
@@ -1,3 +1,28 @@
 $(document).ready(function(){
-  // your code here
+
+var firstNum = parseInt((Math.random()*10+1));
+var secondNum = parseInt((Math.random()*10+1));
+var $userInput = $("#solution-input").val();
+
+  function numberGenerator (){
+    firstNum = parseInt((Math.random()*10+1));
+    secondNum = parseInt((Math.random()*10+1));
+    $("#num1").text(firstNum);
+    $("#num2").text(secondNum);
+
+  }
+
+  function checkAddition(first, second, input){
+    var solution = first+second;
+    if (input===solution){
+      console.log("true");
+    } else console.log("false");
+  }
+
+numberGenerator();
+
+checkAddition(firstNum, secondNum, $userInput);
+
+
+
 });
