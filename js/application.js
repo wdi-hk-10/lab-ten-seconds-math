a9932c375bf15e55f285a367e64e37199c803031
Jon C Young II <youngii.jc@gmail.com>
2016-02-01 00:07:08 +0800

    completed

--- js/application.js
+++ js/application.js
@@ -1,3 +1,68 @@
 $(document).ready(function(){
-  // your code here
+
+  // timer
+  var counter;
+  var count          = 10;
+  var $solutionInput = $('#solution-input');
+
+  // equation
+  var answer;
+  var $num1          = $('#num1');
+  var $num2          = $('#num2');
+  var $secondsLeft   = $('#secondsLeft');
+
+  // checkAnswer
+  var score          = 0;
+
+  // start the game
+  $solutionInput.one('keydown', function() {
+    counter = setInterval(time, 1000);
+    getEquation();
+    checkAnswer();
+  });
+
+  // counter
+  function time() {
+    count--;
+    $secondsLeft.text(count);
+    if (count <= 0) {
+      clearInterval(counter);
+      gameOver();
+    }
+  };
+
+  // set math problem
+  function getEquation(number) {
+    var one = Math.floor(Math.random() * (11 - 1));
+    var two = Math.floor(Math.random() * (11 - 1));
+    $num1.text(one);
+    $num2.text(two);
+
+    answer = one + two;
+  };
+
+  function checkAnswer() {
+    $solutionInput.keyup(function() {
+      var input = parseInt($(this).val());
+      if (input === answer) {
+        correctAnswer();
+      } else {
+        $solutionInput.css('border', 'solid red 1px');
+      }
+    });
+  };
+
+  function correctAnswer() {
+    score += 5;
+    $('#score').text(score);
+    count += 10;
+    $solutionInput.val("");
+    getEquation();
+  };
+
+  function gameOver() {
+    $solutionInput.css('display', 'none');
+    $('#gameover-box').removeClass("hide");
+  };
+
 });
