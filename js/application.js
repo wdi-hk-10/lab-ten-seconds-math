8ff7dd9ee14e9c29ed83ffb473a977846521d960
Julien Waite <julienwaite@gmail.com>
2016-01-31 12:24:04 +0800

    game over complete

--- js/application.js
+++ js/application.js
@@ -1,38 +1,67 @@
 $(document).ready(function(){
   // your code here
 
-  var num1 = 0;
-  var num2 = 0;
+  var sec;
+  var correctAnswer;
+  var score = 0;
+
+  function init () {
+    generateNumbers();
+    clockCountdown();
+    checkAnswer();
+  }
 
   function generateNumbers () {
     var num1 = Math.floor((Math.random() * 10) + 1);
     var num2 = Math.floor((Math.random() * 10) + 1);
-    $("#num1").text(num1);
-    $("#num2").text(num2);
+    $('#num1').text(num1);
+    $('#num2').text(num2);
+    correctAnswer = num1+num2;
+    return correctAnswer;
   }
 
   function clockCountdown () {
-    var sec = 10;
+    sec = 5;
     var timer = setInterval(function() {
-      $("#secondsLeft").text(sec--);
-        if (sec === 0) {
-          $("#gameover-box").delay(1000).fadeIn(1000);
-          $("#answer-box").hide(1000).fadeOut();
+      $('#secondsLeft').text(sec--);
+        if (sec === -1) { // why does this need to be -1, not 0
+          gameOver();
         }
       },1000);
     }
 
+  function checkAnswer () {
+    $('#solution-input').on('input', function() {
+    userAnswer = $('#solution-input').val();
+      if (userAnswer == correctAnswer) {
+        console.log("Well done Perverts");
+        sec = sec + 10;
+        score = score + 5;
+        console.log(userAnswer);
+        generateNumbers();
+        clearInput();
+      }
+      else {
+      console.log("Loser");
+      $('#solution-input').css('border-color', 'red');
+      }
+    });
+  }
 
-//function checkAnswer() {
 
-//}
+  function clearInput () {
+     userAnswer = "";
+  }
 
+  function gameOver () {
+    $('#answer-box').addClass('hidden')//.fadeOut();//
+    $('#gameover-box').removeClass('hidden')//.delay(1000).fadeIn(1000);//
+    $('#score').text(score);
+  }
 
-function init () {
-  generateNumbers();
-  clockCountdown();
-}
-
-init();
+  init();
 
 }); //end of document
+
+// To show it: $("#myId").removeClass('hidden');
+// To hide it: $("#myId").addClass('hidden');
